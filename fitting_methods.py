942c93f385de74f9d1ee4a5a4deb1fd6bcb480a1
pluflou <pluflou@gmail.com>
2021-09-23 18:57:12 -0400

    image analysis and emit calculation done for beamtime test

---
+++ fitting_methods.py
@@ -0,0 +1,99 @@
+import numpy as np
+import scipy.io
+import sys
+import datetime
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+
+def gaussian_linear_background(x, amp, mu, sigma, slope=0, offset=0):
+    """Gaussian plus linear background fn"""
+    return amp*np.exp( -(x-mu)**2/2/sigma**2 ) + slope * x + offset 
+
+def fit_gaussian_linear_background(y, para0 = None, show_plots=False, cut_area = None):
+    """Takes a function y and inputs and fits and Gaussian with
+    linear bg to it. Returns the best fit estimates of the parameters 
+    amp, mu, sigma and their associated 1sig error """
+    from scipy.optimize import curve_fit
+
+    x = np.arange(y.shape[0])
+
+    if para0 == None:
+        offset0 =  np.mean(y[-10:]) #y.min()
+        amp0 = y.max() - offset0
+        mu0 = x[np.argwhere(y==y.max())][0].item() # get the first element if more than two 
+        try:
+            sigma0 = (x[np.argwhere(y>np.exp(-0.5*2**2)*amp0+offset0).max()]\
+                      - x[np.argwhere(y>np.exp(-0.5*2**2)*amp0+offset0).min()] )/(2*2)
+        except:
+            sigma0 = 5
+        slope0 = 0
+        para0 = [amp0, mu0, sigma0, slope0, offset0]
+
+    try:
+        para, para_error = curve_fit(gaussian_linear_background, x, y, p0 = para0)
+    except:
+        print("Fitting failed.")
+        para = para0 
+        para_error = []
+
+    para[2] = abs(para[2])  # Gaussian width is postivie definite
+    # contraints on the output fit parameters
+    if para[2] >= len(x):
+        para[2] = len(x)
+
+    if abs(para[1]) <= 0:
+        para[1] = 0
+
+    if abs(para[1]) >= len(x):
+        para[1] = len(x)
+        
+    if show_plots:
+        plot_fit(x, y, para)
+
+    return para[0:3], np.sqrt(np.diag(para_error))[0:3] 
+
+def find_rms_cut_area(y, para0 = None, show_plots=False, cut_area = 0.05):
+    """Takes a distribution (ndarray) and the desired cut area (5% is default). 
+    Returns the amp (max of array), mean of distribution, and rms (std) of dist"""
+    x = np.arange(y.shape[0])
+    y = np.array([0 if ele < 0 else ele for ele in y])
+
+    cumsum = np.cumsum(y)
+    idLow = int(np.argwhere(cumsum < cut_area/2*cumsum[-1])[-1])
+    idHigh = int(np.argwhere(cumsum > (1-cut_area/2)*cumsum[-1])[0])
+
+    y[0:idLow] = y[idHigh:]=0
+
+    xx = x[y != 0] 
+    xp = y[y != 0]
+
+    mean = np.sum(xx*xp)/ np.sum(xp)
+    mean2 = np.sum(xx*xx*xp)/np.sum(xp)
+    var = mean2 - mean**2
+    std = np.sqrt(var)
+
+    # TODO: better estimate of peak amplitude in case of noise
+    amp = max(y)
+
+    para = np.array([amp, mean, std])
+
+    # TODO: implement errors
+    para_errors = np.array([0]*len(para))
+    
+    if show_plots:
+        plot_fit(x, y, para)
+
+    return para, para_errors
+
+def plot_fit(x, y, para_x, show_plot=True):
+    timestamp = (datetime.datetime.now()).strftime("%m-%d_%H-%M-%S")
+    fig = plt.figure(figsize=(7 ,5))
+    plt.plot(x, y, 'b-', label='data')
+    plt.plot(x, gaussian_linear_background(x, *para_x), 'r-', label='fit: amp=%f, centroid=%f, sigma=%f' % tuple(para_x[:3]))
+    plt.xlabel("Pixel")
+    plt.ylabel("Counts")
+    plt.legend()
+    plt.savefig(f"./plots/beamsize_fit_{timestamp}.png")
+    if show_plot:
+        plt.show()
+    
---
+++ image.py
@@ -0,0 +1,67 @@
+import numpy as np
+import scipy.io
+import sys
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+
+from fitting_methods import *
+
+class Image:
+    def __init__(self, image, ncol, nrow, bg_image = None):
+        self.ncol = ncol
+        self.nrow = nrow
+        self.flat_image = image
+        self.bg_image = bg_image
+        self.offset = 20
+        
+        self.proc_image = None
+        self.x_proj = None
+        self.y_proj = None
+        self.xrms = None
+        self.yrms = None
+        self.xrms_error = None
+        self.yrms_error = None
+        
+    def reshape_im(self, im = None):
+        """Reshapes flattened OTR image to 2D array"""
+        self.proc_image = self.flat_image.reshape(self.ncol,self.nrow)
+        return self.proc_image
+    
+    def subtract_bg(self):
+        """Subtracts bg image"""
+        if self.bg_image is not None:
+            self.bg_image = self.bg_image.reshape(self.ncol,self.nrow)
+            if self.proc_image.shape == self.bg_image.shape:
+                self.proc_image = self.proc_image - self.bg_image
+        return self.proc_image
+
+    def get_im_projection(self, subtract_baseline=True):
+        """Expects ndarray, return x (axis=0) or y (axis=1) projection"""
+        self.x_proj = np.sum(self.proc_image, axis=0)
+        self.y_proj = np.sum(self.proc_image, axis=1)
+        if subtract_baseline:
+            self.x_proj = self.x_proj - np.mean(self.x_proj[0:self.offset])
+            self.y_proj = self.y_proj - np.mean(self.y_proj[0:self.offset])
+        return self.x_proj, self.y_proj    
+            
+    def dispatch(self, name, *args, **kwargs):
+        fit_type_dict = {
+            "gaussian": fit_gaussian_linear_background,
+            "rms cut area": find_rms_cut_area
+            }
+        return fit_type_dict[name](*args, **kwargs)
+
+    def get_sizes(self, method = "gaussian", show_plots = True, cut_area = 0.05):
+        """Takes an image (2D array) and optional bg image, finds x and y projections,
+        and fits with desired method. Current options are "gaussian" or "rms cut area".
+        Returns size in x, size in y, error on x size, error on  y size"""
+
+        # Find statistics
+        para_x, para_error_x = self.dispatch(method, self.x_proj, para0=None, cut_area=cut_area, show_plots=show_plots)
+        para_y, para_error_y = self.dispatch(method, self.y_proj, para0=None, cut_area=cut_area, show_plots=show_plots)
+
+        #      size in x, size in y, error on x size, error on  y size
+        self.xrms, self.yrms, self.xrms_error, self.yrms_error = \
+        para_x[2],  para_y[2], para_error_x[2], para_error_y[2]
+        
+        return self.xrms, self.yrms, self.xrms_error, self.yrms_error 
